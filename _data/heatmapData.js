ddaedb68444a237453c3e7f25a6beb5d03aaa0fa
Chris Bradley <chrbradley (at) gmail.com>
2014-09-19 14:45:10 -0400

    add dummy project data

---
+++ _data/heatmapData.js
@@ -0,0 +1,126 @@
+var glb = glb || {};
+glb.heatmapData	= [
+      {
+      "id" : 5,
+      "item" : "Cookies",
+      "numEvents" : 450,
+      "sales" : 0,
+      "volume" : 0,
+      "margin" : -1,
+      "profit" : -1,
+      "Column" : null,
+      "Column2" : "1 = green ",
+      "Key" : "green = above department average"
+    },
+    {
+      "id" : 4,
+      "item" : "Bath Tissue",
+      "numEvents" : 269,
+      "sales" : 1,
+      "volume" : 1,
+      "margin" : 1,
+      "profit" : 1,
+      "Column" : null,
+      "Column2" : "0 = yellow",
+      "Key" : "yellow = below department average"
+    },
+    {
+      "id" : 8,
+      "item" : "Cereal",
+      "numEvents" : 253,
+      "sales" : 1,
+      "volume" : 1,
+      "margin" : -1,
+      "profit" : -1,
+      "Column" : null,
+      "Column2" : "-1 = red",
+      "Key" : "red = negative"
+    },
+    {
+      "id" : 0,
+      "item" : "Juices",
+      "numEvents" : 190,
+      "sales" : 1,
+      "volume" : 0,
+      "margin" : 1,
+      "profit" : 1,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 6,
+      "item" : "Dressings",
+      "numEvents" : 189,
+      "sales" : 0,
+      "volume" : 0,
+      "margin" : 1,
+      "profit" : 1,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 7,
+      "item" : "Canned Vegetables",
+      "numEvents" : 99,
+      "sales" : 0,
+      "volume" : 1,
+      "margin" : 1,
+      "profit" : 1,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 9,
+      "item" : "Laundry Detergent",
+      "numEvents" : 86,
+      "sales" : 1,
+      "volume" : 1,
+      "margin" : 0,
+      "profit" : 0,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 1,
+      "item" : "Food Wrappers",
+      "numEvents" : 76,
+      "sales" : 1,
+      "volume" : 1,
+      "margin" : 1,
+      "profit" : 1,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 2,
+      "item" : "Drink Mixes",
+      "numEvents" : 30,
+      "sales" : 0,
+      "volume" : 0,
+      "margin" : 0,
+      "profit" : 0,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    },
+    {
+      "id" : 3,
+      "item" : "Household Cleaners",
+      "numEvents" : 17,
+      "sales" : 0,
+      "volume" : 0,
+      "margin" : 1,
+      "profit" : 0,
+      "Column" : null,
+      "Column2" : null,
+      "Key" : null
+    }
+  ]
+
+
+
---
+++ _data/twoByTwoData.js
@@ -0,0 +1,39 @@
+var glb = glb || {};
+glb.twoByTwoData	= {
+"title" : "100% = 110Ads",
+"cells":
+[
+    {
+      "position" : "top_left",
+      "condition" : "warning",
+      "events" : 26,
+      "events_sub" : 21,
+      "incremental_sales" : 32,
+      "incremental_margin" : -16
+    },
+    {
+      "position" : "top_right",
+      "condition" : "success",
+      "events" : 49,
+      "events_sub" : 76,
+      "incremental_sales" : 38,
+      "incremental_margin" : 8
+    },
+    {
+      "position" : "bottom_left",
+      "condition" : "danger",
+      "events" : 26,
+      "events_sub" : 21,
+      "incremental_sales" : 32,
+      "incremental_margin" : -16
+    },
+    {
+      "position" : "bottom_right",
+      "condition" : "warning",
+      "events" : 49,
+      "events_sub" : 76,
+      "incremental_sales" : 38,
+      "incremental_margin" : 8
+    }
+  ]
+}
