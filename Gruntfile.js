34772194fdace126cc5d0ce2d557e06982f4f887
Dan <dnc223@gmail.com>
2014-10-06 22:47:59 -0400

    add and style arrows and buttons

--- Gruntfile.js
+++ Gruntfile.js
@@ -172,7 +172,7 @@ module.exports = function (grunt) {
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
-        browsers: ['last 1 version']
+        browsers: ['none']
       },
       dist: {
         files: [{
