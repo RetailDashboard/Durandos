2217f43cf7891ca43244a7682931c35cb1248ea6
Mike Axtman <mdaxtman@gmail.com>
2014-10-11 10:26:53 -0700

    remove all references to 'thing'

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,37 +5,12 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
 var Category = require('../api/category/category.model');
 var Brand = require('../api/brand/brand.model');
 var Item = require('../api/item/item.model');
 var CategoryMonth = require('../api/categoryMonth/categoryMonth.model');
 var ItemTactic = require('../api/itemTactic/itemTactic.model');
 var Department = require('../api/department/department.model');
-
-
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
-
 
 CategoryMonth.find({}).remove(function() {
   CategoryMonth.create({
